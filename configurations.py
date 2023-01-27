9296449d341945fc93fe4d34d4b7c3cec5649489
hammer-hao <103453499+hammer-hao@users.noreply.github.com>
2023-01-27 10:09:55 +0000

    updated the replay parse method; generated units dataframe

--- configurations.py
+++ configurations.py
@@ -1,6 +1,7 @@
 Units={
     'Protoss':['Probe','Zealot','Stalker','Sentry','Adept','HighTemplar','DarkTemplar','Immortal','Colossus','Disruptor','Archon',
-            'WarpPrism','Phoenix','Observer','VoidRay','Oracle','Carrier','Tempest','Mothership'],
+            'WarpPrism','Phoenix','Observer','VoidRay','Oracle','Carrier','Tempest','Mothership', 'WarpPrismPhasing', 'Interceptor'],
     'Zerg':['Larva','Drone','Queen','Zergling', 'Baneling', 'Roach', 'Ravager', 'Hydralisk', 'Lurker', 'Infestor', 'Swarmhost', 'Ultralist',
-            'Locust', 'Broodling', 'Changeling', 'NydusWorm', 'Overlord', 'Overseer', 'Mutalisk', 'Corruptor', 'BroodLord', 'Viper']
+            'Locust', 'Broodling', 'Changeling', 'NydusWorm', 'Overlord', 'Overseer', 'Mutalisk', 'Corruptor', 'BroodLord', 'Viper', 'Egg',
+            'OverlordCocoon', 'BanelingCocoon', 'LurkerMPEgg', 'LurkerMP', 'LurkerMPBurrowed', 'SwarmHostMP', 'LocustMPFlying', 'LocustMP', 'LocustMPPrecursor']
     }
--- replay.py
+++ replay.py
@@ -1,22 +1,60 @@
 from zephyrus_sc2_parser import parse_replay
 import configurations
 import pandas as pd
+from pathlib import Path
+from zephyrus_sc2_parser import parse_replay
+import tqdm
 
-'''
-class unit:
-    def __init__(self, name, live, died, in_progress):
-        self.name=name
-        self.live=live
-        self.died=died
-        self.in_progress=in_progress
-    def append(self):
-        return [self.name, self.live, self.died, self.in_progress]
-'''
-game=parse_replay('test.SC2Replay', tick=100)
+# change this to match the directory you're using
+replays = Path('C:/Users/hammerhao/OneDrive/Documents/StarCraft II/Accounts/818931843/2-S2-1-10228956/Replays/Multiplayer')
 
-def generate_snapshot(thisgame):
-    timeline=thisgame[1]
-    metadata=thisgame[3]
+def recursereplays(dir_path):
+    """
+    Recursively searches directories to parse replay files
+    """
+    dir_list=list(dir_path.iterdir())
+    print(dir_list)
+    replay_list=[]
+    for obj_path in tqdm.tqdm(dir_list):
+        if obj_path.is_file():
+            try:
+                replay = parse_replay(obj_path)
+                replay_list.append(replay)
+                # do stuff with the data
+            except:
+                pass
+    return replay_list
+
+replay_list = recursereplays(replays)
+
+def generate_unit_snapshot(snapshot, player_codes, protosswin):
+    #generate empty unit data
+    totalunits={}
+    for i in range(2):
+        unitsdict={}
+        for unitname in configurations.Units[player_codes[i+1]]:
+            live=unitname+'_live'
+            died=unitname+'_dead'
+            inprogress=unitname+'_in_progress'
+            thisunit=[{live:0}, {died:0}, {inprogress:0}]
+            for dic in thisunit:
+                unitsdict.update(dic)
+        #find matching unit
+        unitdetails=snapshot[i+1]['unit']
+        for thisunitname, details in unitdetails.items():
+            live=thisunitname+'_live'
+            died=thisunitname+'_dead'
+            inprogress=thisunitname+'_in_progress'
+            ingameunitstats=[{live:details['live']}, {died:details['died']}, {inprogress:details['in_progress']}]
+            for dic in ingameunitstats:
+                unitsdict.update(dic)
+        totalunits.update(unitsdict)
+        totalunits.update({'Protosswin':protosswin})
+        totalunits.update
+    return totalunits
+
+def gen_total_timeline(game):
+    metadata=game[3]
     if 'creep' in metadata['race'][1]:
         player_codes={
             1:'Zerg',
@@ -27,25 +65,22 @@ def generate_snapshot(thisgame):
             1:'Protoss',
             2:'Zerg'
         }
-    units_snapshot=timeline[100]
-    #generate empty unit data
-    totalunits={}
-    for i in range(2):
-        unitsdict={}
-        for unitname in configurations.Units[player_codes[i+1]]:
-            thisunit={unitname:[0, 0, 0]}
-            unitsdict.update(thisunit)
+    protosswin=player_codes[game[4]['winner']]=='Protoss'
+    timeline=game[1]
+    total_timeline = [generate_unit_snapshot(timepoint, player_codes, protosswin) for timepoint in timeline]
+    return total_timeline
 
-        #find matching unit
-        unitdetails=units_snapshot[i+1]['unit']
-        for thisunitname, details in unitdetails.items():
-            ingameunitstats={thisunitname:[details['live'], details['died'], details['in_progress']]}
-            unitsdict.update(ingameunitstats)
-        totalunits.update(unitsdict)
-    return totalunits
+pvz_replay_list=[]
+for replay in replay_list:
+    if ('creep' in replay[3]['race'][1]) | ('creep' in replay[3]['race'][2]):
+        pvz_replay_list.append(replay)
 
-print(generate_snapshot(game))
+totalunits=[]
+for replay in tqdm.tqdm(pvz_replay_list):
+    totalunits+=gen_total_timeline(replay)
 
+df = pd.DataFrame(totalunits)
+df.to_csv('pvzgames.csv')
 '''
 class replay:
     def __init__(self, link_to_replay):
