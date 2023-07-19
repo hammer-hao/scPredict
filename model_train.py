1db7b8523952434c87433e6a173c17ac01bee8e8
Zihe Hao <103453499+hammer-hao@users.noreply.github.com>
2023-07-19 09:53:59 +0800

    added first distributable predictor

--- model_train.py
+++ model_train.py
@@ -24,71 +24,25 @@ replay_list = replay.recursereplays(replays)
 with open('D:/sc2models/parsed_replays', 'wb') as file:
     pickle.dump(replay_list, file)
 
-with open('parsed_replays', 'rb') as file:
+with open('D:/sc2models/parsed_replays', 'rb') as file:
     replay_list = pickle.load(file)
+
+features={
+    'pvp':training_features.pvp,
+    'pvz':training_features.pvz,
+    'pvt':training_features.pvt,
+    'zvt':training_features.zvt,
+    'tvt':training_features.tvt,
+    'zvz':training_features.zvz
+}
 
 #generate timeline as dataframe and save as csv
 for matchup, matchup_replays in replay_list.items():
-    games_data=matchup_replays
+    matchup_features=features[matchup]
+    columns_bool=matchup_replays.columns.isin(matchup_features)
+    filtered_data=matchup_replays.loc[:,columns_bool]
     train_file_name='data/'+str(matchup)+'_train.csv'
-    games_data=games_data.fillna(0)
-    games_data.to_csv(train_file_name)
-
-pvp_data=replay_list['pvp']
-pvp_features=training_features.pvp
-columns_bool=pvp_data.columns.isin(pvp_features)
-pvp_data_filter=pvp_data.loc[:, columns_bool]
-pvp_data_filter.to_csv('pvp_train.csv')
-
-pvt_data=pd.read_csv('data/pvt_train.csv', index_col=0)
-pvt_features=training_features.pvt
-columns_bool=pvt_data.columns.isin(pvp_features)
-pvt_data_filter=pvt_data.loc[:, columns_bool]
-#pvt_data_filter.iloc[:, 0:8]=np.log(pvt_data_filter.iloc[:, 0:8]+1)
-pvt_data_filter.to_csv('pvt_train.csv')
-
-#RFC model for each matchup
-
-for matchup in replay_list.keys():
-    data_path='data/'+str(matchup)+'_train.csv'
-    games=pd.read_csv(data_path)
-    try:
-        games["result"] = games["result"].astype(int)
-        features = games.drop('result', axis=1)
-        results = games['result']
-
-        # implementing train-test-split, not required for applying the model
-        #X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=66)
-
-        # random forest model creation
-        rfc = ensemble.RandomForestClassifier(n_estimators=1500, n_jobs=-1)
-        rfc.fit(features,results)
-        filename='models/rfc_'+str(matchup)+'.model'
-        pickle.dump(rfc, open(filename, 'wb'))
-    except KeyError:
-        pass
-
-games=pd.read_csv('pvp_train.csv', index_col=0).fillna(0)
-games.loc[:,"result"] = games["result"].astype(int)
-features = games.drop('result', axis=1)
-results = games['result']
-rfc = ensemble.RandomForestClassifier(n_estimators=1500, n_jobs=-1, class_weight='balanced', max_features='sqrt')
-rfc.fit(features,results)
-filename='pvp_test.model'
-pickle.dump(rfc, open(filename, 'wb'))
-
-games=pd.read_csv('pvt_train.csv', index_col=0).fillna(0)
-games.loc[:,"result"] = games["result"].astype(int)
-features = games.drop('result', axis=1)
-results = games['result']
-rfc = ensemble.RandomForestClassifier(n_estimators=1500, n_jobs=-1, class_weight='balanced', max_features='sqrt')
-rfc.fit(features,results)
-filename='D:/sc2models/pvt_test.model'
-pickle.dump(rfc, open(filename, 'wb'))
-
-logit = linear_model.LogisticRegression(penalty='l2', n_jobs=-1)
-logit.fit(features,results)
-pickle.dump(logit, open('D:/sc2models/pvt_test_logit.model', 'wb'))
+    filtered_data.to_csv(train_file_name)
 
 #interact all variables with gameloop
 def make_interactions(df, root):
@@ -102,14 +56,25 @@ def make_interactions(df, root):
     X_interactions = pd.concat([df, interaction_terms], axis=1)
     return X_interactions
 
-features_interactions=make_interactions(features, 'loop')
+for matchup in ['pvp', 'pvz', 'pvt', 'zvt', 'zvz', 'tvt']:
+    data_path='data/'+matchup+'_train.csv'
+    games=pd.read_csv(data_path, index_col=0).fillna(0)
+    games.loc[:,"result"] = games["result"].astype(int)
+    features = games.drop('result', axis=1)
+    results = games['result']
 
-logit_interactions = linear_model.LogisticRegression(penalty='l2', n_jobs=-1, max_iter=250)
-logit_interactions.fit(features_interactions,results)
-pickle.dump([logit_interactions,features_interactions.columns], open('D:/sc2models/pvt_test_logit_w_interactions.model', 'wb'))
+    #logit
+    logit = linear_model.LogisticRegression(penalty='l2', n_jobs=-1)
+    logit.fit(features,results)
+    savepath='models/'+matchup+'_logit.model'
+    pickle.dump(logit, open(savepath, 'wb'))
 
-# X_interactions will have additional features representing the interactions
-print(X_interactions)
+    #logit with interactions
+    features_interactions=make_interactions(features, 'loop')
+    logit_interactions = linear_model.LogisticRegression(penalty='l2', n_jobs=-1, max_iter=250)
+    logit_interactions.fit(features_interactions,results)
+    savepath_temporal='models/'+matchup+'_logit_temporal.model'
+    pickle.dump([logit_interactions,features_interactions.columns], open(savepath_temporal, 'wb'))
 
 # predictions
 rfc_predict = rfc.predict(X_test)
--- pvtpredictor.py
+++ pvtpredictor.py
@@ -2,26 +2,64 @@ from tkinter import *
 from tkinter import filedialog
 from pickle import load as pickleload
 from zephyrus_sc2_parser import parse_replay
-from training_features import pvt as pvtfeatures
+import training_features
 from replay import gen_total_timeline
 from pandas import DataFrame, concat
+from sklearn import linear_model
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
 import sys
 import os
 
+#resource path for packaging
 def resource_path(relative_path):
     if hasattr(sys, '_MEIPASS'):
         return os.path.join(sys._MEIPASS, relative_path)
     return os.path.join(os.path.abspath("."), relative_path)
 
-file = open(resource_path('pvt_test_logit_w_interactions.model'),'rb')
-pvt_logitint=pickleload(file)
-file.close()
+#model dictionary
+model_dict={}
+for matchup in ['pvt', 'pvz', 'zvt', 'tvt', 'zvz', 'pvp']:
+    model_path='models/'+matchup+'_logit_temporal.model'
+    file = open(resource_path(model_path),'rb')
+    thismodel=pickleload(file)
+    model_dict.update({matchup:thismodel})
+    file.close()
 
-df_dict={}
-for feature in pvtfeatures:
-    df_dict.update({feature:0})
+features_dict={
+    'pvp':training_features.pvp,
+    'pvz':training_features.pvz,
+    'pvt':training_features.pvt,
+    'zvt':training_features.zvt,
+    'tvt':training_features.tvt,
+    'zvz':training_features.zvz
+}
+
+#get matchup from replay
+def get_matchup(gamepath):
+    replay=parse_replay(gamepath, local=True, tick=90)
+    try:
+        player1_workername=list(replay[1][2][1]['unit'].keys())[0]
+        player2_workername=list(replay[1][2][2]['unit'].keys())[0]
+        player_workertypes=[player1_workername, player2_workername]
+        if 'SCV' in player_workertypes:
+                    if 'Probe' in player_workertypes:
+                        matchup='pvt'
+                    elif ('Egg' in player_workertypes)|(('Larva') in player_workertypes):
+                        matchup='zvt'
+                    elif ('Probe' not in player_workertypes) & ('Egg' not in player_workertypes) & ('Larva' not in player_workertypes):
+                        matchup='tvt'
+        elif 'Probe' in player_workertypes:
+                    if ('Egg' in player_workertypes)|(('Larva') in player_workertypes):
+                        matchup='pvz'
+                    elif ('Egg' not in player_workertypes)&(('Larva') not in player_workertypes):
+                        matchup='pvp'
+        elif ('Egg' in player_workertypes)|(('Larva') in player_workertypes):
+                    matchup='zvz'
+    except IndexError:
+        matchup='None'
+    del replay
+    return matchup
 
 def make_interactions(df, root):
     feature = root
@@ -34,42 +72,177 @@ def make_interactions(df, root):
     X_interactions = concat([df, interaction_terms], axis=1)
     return X_interactions
 
-def predict_with_int(game, model):
+def predict_with_int(game, model, matchup):
+    df_dict={}
+    features_total=features_dict[matchup]
+    for feature in features_total:
+        df_dict.update({feature:0})
+    print(df_dict)
     thisreplay=parse_replay(game, local=True, tick=90)
-    totallist=[df_dict]+gen_total_timeline(thisreplay, 'pvt')
+    totallist=[df_dict]+gen_total_timeline(thisreplay, matchup)
     timeline=DataFrame(totallist).drop(['result'], axis=1).fillna(0)
-    timeline_int=make_interactions(timeline, 'loop')
-    predictions = model.predict_proba(timeline_int)
+    columns_bool=timeline.columns.isin(features_total)
+    filtered_timeline=timeline.loc[:,columns_bool]
+    timeline_int=make_interactions(filtered_timeline, 'loop')
+    index=list(model[1])
+    timeline_int=timeline_int[index]
+    predictions = model[0].predict_proba(timeline_int)
     winratearr = [(1-percentage[0]) for percentage in predictions]
     return winratearr, timeline
 
 def plot(data):
+    global axvline
     ax.clear()
     ax.plot(data)
     ax.set_xlabel('gameloop')
     ax.set_ylabel('winrate')
     ax.set_title('winrate by gameloop')
+    axvline=ax.axvline(x=currentloop, linestyle='dashed')
 
 window = Tk()
+window.minsize(300,100)
+window.maxsize(300,100)
 fig, ax = plt.subplots()
+
+currentloop=0
+
+def go_forward():
+    global currentloop
+    global canvas
+    global axvline
+    global details
+    global details_label
+    currentloop+=1
+    axvline.set_data([currentloop, currentloop], [0,1])
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+    details_label='current gameloop: '+str(currentloop)+'   gametime: '+str((currentloop*5)//60)+':'+str((currentloop*5)%60)+'   winrate: '+str(logit_winrates_with_interactions[currentloop])
+    details['text']=details_label
+    
+
+def go_backward():
+    global currentloop
+    global canvas
+    global axvline
+    global details
+    global details_label
+    currentloop-=1
+    axvline.set_data([currentloop, currentloop], [0,1])
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+    details_label='current gameloop: '+str(currentloop)+'   gametime: '+str((currentloop*5)//60)+':'+str((currentloop*5)%60)+'   winrate: '+str(logit_winrates_with_interactions[currentloop])
+    details['text']=details_label
+
+def go_forward_timesfive():
+    global currentloop
+    global canvas
+    global axvline
+    global details
+    global details_label
+    currentloop+=5
+    axvline.set_data([currentloop, currentloop], [0,1])
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+    details_label='current gameloop: '+str(currentloop)+'   gametime: '+str((currentloop*5)//60)+':'+str((currentloop*5)%60)+'   winrate: '+str(logit_winrates_with_interactions[currentloop])
+    details['text']=details_label
+    
+
+def go_backward_timesfive():
+    global currentloop
+    global canvas
+    global axvline
+    global details
+    global details_label
+    currentloop-=5
+    axvline.set_data([currentloop, currentloop], [0,1])
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+    details_label=('current gameloop: '+str(currentloop)+
+    '   gametime: '+str((currentloop*5)//60)+':'+str((currentloop*5)%60)+
+    '   winrate: '+str(logit_winrates_with_interactions[currentloop]))
+    details['text']=details_label
+
+def go_to_start():
+    global currentloop
+    global canvas
+    global axvline
+    global details
+    global details_label
+    currentloop=0
+    axvline.set_data([currentloop, currentloop], [0,1])
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+    details_label=('current gameloop: '+str(currentloop)+
+    '   gametime: '+str((currentloop*5)//60)+':'+str((currentloop*5)%60)+
+    '   winrate: '+str(logit_winrates_with_interactions[currentloop]))
+    details['text']=details_label
+
+def go_to_end():
+    global currentloop
+    global canvas
+    global axvline
+    global gamelength
+    global details
+    global details_label
+    currentloop=gamelength
+    axvline.set_data([currentloop, currentloop], [0,1])
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+    details_label=('current gameloop: '+str(currentloop)+
+    '   gametime: '+str((currentloop*5)//60)+':'+str((currentloop*5)%60)+
+    '   winrate: '+str(logit_winrates_with_interactions[currentloop]))
+    details['text']=details_label
 
 def openfile():
     global canvas
+    global currentloop
+    global gamelength
+    global logit_winrates_with_interactions
+    global details
+    global details_label
     path = filedialog.askopenfilename()
-    logit_winrates_with_interactions, game_details_logitint=predict_with_int(path, pvt_logitint)
+    matchup=get_matchup(path)
+    model=model_dict[matchup]
+    logit_winrates_with_interactions, game_details=predict_with_int(path, model, matchup=matchup)
+    gamelength=len(logit_winrates_with_interactions)
     top = Toplevel()
+
+    top.minsize(640,640)
+    top.maxsize(640,640)
     top.title('Win rate progression')
     label = Label(text='win rates progression', master=top)
     label.config(font=('Courier', 32))
     label.pack()
+
     canvas = FigureCanvasTkAgg(fig, master=top)
     winrateplot=plot(logit_winrates_with_interactions)
+    currentloop=0
     canvas.draw()
     canvas.get_tk_widget().pack()
+    
+    controls=Frame(master=top)
+    controls.pack()
+    front=Button(text='Start', command=go_to_start, master=controls)
+    forward_timesfive=Button(text='>>', command=go_forward_timesfive, master=controls)
+    forward=Button(text='>', command=go_forward, master=controls)
+    backward=Button(text='<', command=go_backward, master=controls)
+    backward_timesfive=Button(text='<<', command=go_backward_timesfive, master=controls)
+    end=Button(text='End', command=go_to_end, master=controls)
+    front.pack(side=LEFT)
+    backward_timesfive.pack(side=LEFT)
+    backward.pack(side=LEFT)
+    forward.pack(side=LEFT)
+    forward_timesfive.pack(side=LEFT)
+    end.pack(side=LEFT)
+    details_label=('current gameloop: '+str(currentloop)+
+    '   gametime: '+str((currentloop*5)//60)+':'+str((currentloop*5)%60)+
+    '   winrate: '+str(logit_winrates_with_interactions[currentloop]))
+    details=Label(master=top, text=details_label)
+    details.pack(side=BOTTOM)
 
     return path
 
-button = Button(text='Select PVT replay', command=openfile)
+button = Button(text='Select any 1v1 replay', command=openfile)
 button.pack()
 
 window.mainloop()
