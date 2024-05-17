7b0a8bb86ab64e3018c862da45bfd515e5c24d47
hammer-hao <103453499+hammer-hao@users.noreply.github.com>
2024-05-17 18:59:08 -0400

    updated server side model to logit

--- scpredict/scpredict.py
+++ scpredict/scpredict.py
@@ -4,6 +4,7 @@ import pandas as pd
 import pickle
 from dotenv import load_dotenv
 import os
+import warnings
 
 class Predictor():
     def __init__(self):
@@ -13,19 +14,21 @@ class Predictor():
         self.matchups = matchups
         self.parser = ZGameParser()
 
-        with open(f'{self.model_path}/feature_names.pkl') as f:
+        with open(f'{self.model_path}/feature_names.pkl', 'rb') as f:
             self.feature_names = pickle.load(f)
 
         for matchup in self.matchups:
-            with open(f'{self.model_path}/{matchups}.pkl') as f:
+            with open(f'{self.model_path}/{matchup}.pkl', 'rb') as f:
                 self.models[matchup] = pickle.load(f)
         
     def predict(self, path_to_replay):
         timeline, matchup = self.parser.parse(path=path_to_replay)
         prediction_features = pd.DataFrame(columns=self.feature_names[matchup])
-        for entry in timeline:
-            row = {feature: entry.get(feature, None) for feature in self.feature_names[matchup]}
-            prediction_features = prediction_features.append(row, ignore_index=True)
+        with warnings.catch_warnings():
+            warnings.simplefilter(action='ignore', category=FutureWarning)
+            for entry in timeline:
+                row = {feature: entry.get(feature, None) for feature in self.feature_names[matchup]}
+                prediction_features = prediction_features.append(row, ignore_index=True)
         prediction_features = prediction_features.fillna(0)
         logit_predictions = self.models[matchup].predict_proba(prediction_features)[:, -1]
         self.parser.reset()
--- server.py
+++ server.py
@@ -1,23 +1,25 @@
 import os
 from flask import Flask, flash, request, redirect, url_for, make_response
+from flask_restful import Api, Resource
 from werkzeug.utils import secure_filename
-from scpredict.scpredict import mlp_predict
+from scpredict.scpredict import Predictor
 
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'sc2replay'}
 
+predictor = Predictor()
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.secret_key = 'your_secret_key_here'
 
-def response(message):
-    resp = make_response(message)
-    resp.headers['Access-Control-Allow-Origin'] = '*'
-    return resp
+api = Api(app)
 
+'''
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -38,10 +40,36 @@ def upload_file():
             path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             #print(path)
             file.save(path)
-            out = str(mlp_predict(path))
+            out = str(predictor.predict(path))
             os.remove(path)
             return response(out)
     return response('suck my dick')
+'''
+
+class ReplayHandler(Resource):
+
+    def allowed_file(self, filename):
+        return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    
+    def post(self):
+        # Get Text type fields
+        form = request.form.to_dict()
+        print(form)
+
+        if 'file' not in request.files:
+            return 'No file part'
+        
+        file = request.files.get("file")
+        if file and self.allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(path)
+            out = str(list(predictor.predict(path)))
+            os.remove(path)
+            return {'winrates': out}
+
+api.add_resource(ReplayHandler, "/")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
