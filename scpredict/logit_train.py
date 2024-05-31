0c7bbd7c312059d414d3cc2372c4acd41c8fe263
Zihe Hao <103453499+hammer-hao@users.noreply.github.com>
2024-05-31 16:37:36 +0800

    added shap pkl explainers

--- scpredict/logit_train.py
+++ scpredict/logit_train.py
@@ -1,9 +1,10 @@
 from sklearn.linear_model import LogisticRegression
 import pandas as pd
-from scpredict.settings import *
+from settings import *
 import pickle
 from dotenv import load_dotenv
 import os
+import shap
 
 load_dotenv()
 model_path = os.environ.get('PATHTOMODELS')
@@ -22,5 +23,19 @@ def train_all():
     with open(f'{model_path}/feature_names.pkl', 'wb') as f:
         pickle.dump(feature_names, f)
 
+
+def generate_explainers():
+    for matchup in matchups:
+        data_path = f'replays_processed/zephyrus/{matchup}.csv'
+        matchup_data = pd.read_csv(data_path).fillna(0).drop(['winner'], axis=1)
+        model_path = f'saved_models/logit/{matchup}.pkl'
+        with open(model_path, 'rb') as f:
+            model = pickle.load(f)
+        explainer = shap.Explainer(model, matchup_data)
+        shap_path = f'saved_models/shap/{matchup}_shap.pkl'
+        with open(shap_path, 'wb') as f:
+            pickle.dump(explainer, f)
+
 if __name__ == "__main__":
     train_all()
+    generate_explainers()
